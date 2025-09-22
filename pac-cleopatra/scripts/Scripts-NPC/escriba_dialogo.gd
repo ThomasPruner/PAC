5b2cddcab7f54e167e6a0a003ef765c396a3f0e9
Sophikgr <rubemkruger123@gmail.com>
2025-09-22 07:37:24 -0300

    Estilização da página do escriba + assets

--- pac-cleopatra/scripts/Scripts-NPC/escriba_dialogo.gd
+++ pac-cleopatra/scripts/Scripts-NPC/escriba_dialogo.gd
@@ -1,52 +1,22 @@
 extends Control
 
-# Dados do diálogo
-var question_data = {
-	"question": "Qual é o deus do sol no Egito Antigo?",
-	"answers": ["Rá", "Hórus", "Anúbis", "Ísis"]
-}
-
 func _ready():
-	# Fundo
-	var fundo = TextureRect.new()
-	fundo.texture = load("res://imagens/templo_egipcio.png")
-	fundo.expand = true
-	fundo.stretch_mode = TextureRect.STRETCH_SCALE
-	fundo.anchor_right = 1.0
-	fundo.anchor_bottom = 1.0
-	add_child(fundo)
 
-	# Caixa principal
-	var margin = MarginContainer.new()
-	margin.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	margin.add_theme_constant_override("margin_top", 50)
-	margin.add_theme_constant_override("margin_bottom", 50)
-	margin.add_theme_constant_override("margin_left", 200)
-	margin.add_theme_constant_override("margin_right", 200)
-	add_child(margin)
+	$VBoxContainer/Button.text = "Rio Amazonas"
+	$VBoxContainer/Button2.text = "Rio Tietê"
+	$VBoxContainer/Button3.text = "Rio Nilo"
+	$VBoxContainer/Button4.text = "Rio Ísis"
 
-	var vbox = VBoxContainer.new()
-	margin.add_child(vbox)
-
-	# Pergunta
-	var label_question = Label.new()
-	label_question.text = question_data["question"]
-	label_question.autowrap_mode = TextServer.AUTOWRAP_WORD
-	label_question.add_theme_font_size_override("font_size", 28)
-	vbox.add_child(label_question)
-
-	# Respostas
-	var answers_box = VBoxContainer.new()
-	answers_box.add_theme_constant_override("separation", 10)
-	vbox.add_child(answers_box)
-
-	for i in range(question_data["answers"].size()):
-		var button = Button.new()
-		button.text = question_data["answers"][i]
-		button.pressed.connect(_on_answer_pressed.bind(i))
-		answers_box.add_child(button)
+func _on_button_pressed() -> void:
+	print("Errou")
 
 
-func _on_answer_pressed(answer_index: int):
-	var resposta = question_data["answers"][answer_index]
-	print("Jogador escolheu:", resposta)
+func _on_button_2_pressed() -> void:
+	print("Errou")
+
+
+func _on_button_3_pressed() -> void:
+	print("Acertou")
+	
+func _on_button_4_pressed() -> void:
+	print("Errou")
