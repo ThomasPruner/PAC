68544936a49b71bfbb08298110c9e7263137967b
Thomas Pruner <thomaspruner@hotmail.com>
2025-09-27 15:42:13 -0300

    Perguntas alteradas e corrigidas

--- pac-cleopatra/scripts/Scripts-NPC/comerciante_alimentos_dialogo.gd
+++ pac-cleopatra/scripts/Scripts-NPC/comerciante_alimentos_dialogo.gd
@@ -2,10 +2,10 @@ extends Control
 
 func _ready():
 
-	$VBoxContainer/Button.text = "Rio Amazonas"
-	$VBoxContainer/Button2.text = "Rio Tietê"
-	$VBoxContainer/Button3.text = "Rio Nilo"
-	$VBoxContainer/Button4.text = "Rio Ísis"
+	$VBoxContainer/Button.text = "Pão e tâmaras"
+	$VBoxContainer/Button2.text = "Lentilhas"
+	$VBoxContainer/Button3.text = "Trigo"
+	$VBoxContainer/Button4.text = "Um copo de água"
 
 func show_feedback(texto: String, cor: Color) -> void:
 	$LabelFeedback.text = texto
@@ -13,13 +13,14 @@ func show_feedback(texto: String, cor: Color) -> void:
 	$LabelFeedback.visible = true
 
 func _on_button_pressed() -> void:
-	show_feedback("Errou!", Color.RED)
-
-func _on_button_2_pressed() -> void:
 	show_feedback("Acertou!", Color.GREEN)
 	await get_tree().create_timer(2.0).timeout  # espera 2 segundos
 	GlobalVars.acertouAlimento = true
 	get_tree().change_scene_to_file("res://cenas/main_primeira_fase.tscn")
+	
+
+func _on_button_2_pressed() -> void:
+	show_feedback("Errou!", Color.RED)
 
 func _on_button_3_pressed() -> void:
 	show_feedback("Errou!", Color.RED)
--- pac-cleopatra/scripts/Scripts-NPC/joalheiro_dialogo.gd
+++ pac-cleopatra/scripts/Scripts-NPC/joalheiro_dialogo.gd
@@ -2,10 +2,10 @@ extends Control
 
 func _ready():
 
-	$VBoxContainer/Button.text = "Rio Amazonas"
-	$VBoxContainer/Button2.text = "Rio Tietê"
-	$VBoxContainer/Button3.text = "Rio Nilo"
-	$VBoxContainer/Button4.text = "Rio Ísis"
+	$VBoxContainer/Button.text = "Um escaravelho"
+	$VBoxContainer/Button2.text = "Um olho de Hórus"
+	$VBoxContainer/Button3.text = "Uma corda"
+	$VBoxContainer/Button4.text = "Uma foice"
 
 func show_feedback(texto: String, cor: Color) -> void:
 	$LabelFeedback.text = texto
@@ -16,13 +16,14 @@ func _on_button_pressed() -> void:
 	show_feedback("Errou!", Color.RED)
 
 func _on_button_2_pressed() -> void:
-	show_feedback("Errou!", Color.RED)
-
-func _on_button_3_pressed() -> void:
 	show_feedback("Acertou!", Color.GREEN)
 	await get_tree().create_timer(2.0).timeout  # espera 2 segundos
 	GlobalVars.acertouJoalheiro = true
 	get_tree().change_scene_to_file("res://cenas/main_primeira_fase.tscn")
+	
+
+func _on_button_3_pressed() -> void:
+	show_feedback("Errou!", Color.RED)
 
 func _on_button_4_pressed() -> void:
 	show_feedback("Errou!", Color.RED)
